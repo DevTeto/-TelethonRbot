d846a418dd46044c5f9cd389480042a975cfb46d
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-04-08 01:26:56 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+from mody import Mody
+from pyrogram import Client,filters,enums
+import redis
+r = redis.Redis(
+    host="127.0.0.1",
+    port=6379,
+    charset="utf-8",
+    decode_responses=True
+)
+
+sudo_id = 6352598131
+bot_user = Mody.BOT_USER
+via_user = Mody.VIA_USER
+elhyba = bot_user
+via = via_user
+api_id = Mody.APP_ID
+api_hash = Mody.API_HASH
+session = Mody.SESSION
+token = Mody.TG_BOT_TOKEN
+sudo_command = [6352598131]
+pm =  Mody.MENTION
+mention = "6352598131"
+plugins = dict(root="plugins")
+app = Client(via,api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
+bot = Client(elhyba,api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
